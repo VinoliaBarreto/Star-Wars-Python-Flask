21e47562c78ba6e971b2058c086c2bef9528ded5
VinoliaBarreto <vinolia.barreto@gmail.com>
2021-08-13 10:43:39 +0000

    Adding data base Star Wars

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,87 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    userID = Column(Integer, primary_key=True)
+    userName = Column(String(20), unique=True, nullable=False)
+    favoriteID = Column(Integer, ForeignKey('favorite.favoriteID'))
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    favorite = relationship('Favorite', back_populates = 'user', uselist = False )
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    favoriteID = Column(Integer, unique =True)
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planetsID = Column(String(20), unique=True, nullable=False)
+    speciesID = Column(String(20), unique=True, nullable=False)
+    vehiclesID = Column(String(20), unique=True, nullable=False)
+    userID = Column(Integer, ForeignKey('user.userID'))
+    user = relationship('User', back_populates="favorite")
 
-    def to_dict(self):
-        return {}
+class People(Base):
+    __tablename__ = 'people'
+    peopleID = Column(Integer, unique = True, primary_key=True, nullable = False)
+    uid = Column(Integer, unique = True, nullable = False)
+    speciesName = Column(String, unique = True, nullable = False)
+    description = Column(String, unique = True, nullable = False)
+    planetID = Column(Integer, unique = True, nullable = False)
+    height = Column(Integer, unique = True, nullable = False)
+    mass = Column(Integer, unique = True, nullable = False)
+    hair_color = Column(String, unique = True, nullable = False)
+    skin_color = Column(String, unique = True, nullable = False)
+    eye_color = Column(String, unique = True, nullable = False)
+    birth_year = Column(String, unique = True, nullable = False)
+    gender = Column(String, unique = True, nullable = False)
+    created = Column(String, unique = True, nullable = False)
+    edited = Column(String, unique = True, nullable = False)
+    url = Column(String, unique = True, nullable = False)
+    favoriteID = Column(Integer, ForeignKey('favorite.favoriteID'))
+    favorite = relationship(User)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    planetID = Column(Integer, unique = True, primary_key=True, nullable = False)
+    uid = Column(Integer, unique = True, nullable = False)
+    planetName = Column(Integer, unique = True, nullable = False)
+    description = Column(String, unique = True, nullable = False)
+    diameter = Column(Integer, unique = True, nullable = False)
+    rotation_period = Column(Integer, unique = True, nullable = False)
+    orbital_period = Column(Integer, unique = True, nullable = False)
+    gravity = Column(Integer, unique = True, nullable = False)
+    population = Column(Integer, unique = True, nullable = False)
+    climate = Column(Integer, unique = True, nullable = False)
+    terrain = Column(Integer, unique = True, nullable = False)
+    surface_water = Column(Integer, unique = True, nullable = False)
+    created = Column(String, unique = True, nullable = False)
+    edited = Column(String, unique = True, nullable = False)
+    url = Column(String, unique = True, nullable = False)
+    favoriteID = Column(Integer, ForeignKey('favorite.favoriteID'))
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    shipID = Column(Integer, unique = True, primary_key=True, nullable = False)
+    shipName = Column(String, unique = True, nullable = False)
+    description = Column(String, unique = True, nullable = False)
+    model = Column(String, unique = True, nullable = False)
+    starship_class = Column(String, unique = True, nullable = False)
+    manufacturer = Column(String, unique = True, nullable = False)
+    cost_in_credits = Column(Integer, unique = True, nullable = False)
+    length = Column(Integer, unique = True, nullable = False)
+    crew = Column(Integer, unique = True, nullable = False)
+    passengers = Column(Integer, unique = True, nullable = False)
+    max_atmosphering_speed = Column(Integer, unique = True, nullable = False)
+    hyperdrive_rating = Column(Integer, unique = True, nullable = False)
+    MGLT = Column(Integer, unique = True, nullable = False)
+    cargo_capacity = Column(Integer, unique = True, nullable = False)
+    consumables = Column(String, unique = True, nullable = False)
+    pilots = Column(String, unique = True, nullable = False)
+    created = Column(Integer, unique = True, nullable = False)
+    edited = Column(String, unique = True, nullable = False)
+    favoriteID = Column(Integer, ForeignKey('favorite.favoriteID'))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
